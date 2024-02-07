5fa728259a6a6218158750ffd5c2c95d6bd52afe
Sparrow <149317656+Sparrow3699@users.noreply.github.com>
2024-02-07 18:57:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6855583104:AAE6zslM3pa_mZfM-_pZLXVtIuqNHv9DgvY")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6936006796:AAHBnOPICZscmW5rrNAVf0Gn4zYgp0zkqCs")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "27501733"))
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "27501733"))
 API_HASH = os.environ.get("API_HASH", "687b5c7656849e9a3b125d691f824497")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001568260061"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001836679476"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5847742709"))
@@ -28,11 +28,11 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5847742709"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://lajopoc569:suryabhai3699@cluster0.z3mprke.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "privatesharefilesdb")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://munnabhaiya3699:munna7268@cluster0.rvpqi9a.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "SmartSavingsClubDB")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = {int(_id) for _id in os.environ('FORCE_SUB_CHANNEL', '').split() if _id and _id.startswith('-100')}
+FORCE_SUB_CHANNEL = {int(_id) for _id in os.environ('FORCE_SUB_CHANNEL', '-1001976708417 -1001824793325').split() if _id and _id.startswith('-100')}
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join in my Channels to get files\n\nKindly Please join Channel</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
