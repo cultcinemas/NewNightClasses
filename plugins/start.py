d348ee5e2df22f56b30fbfe9db1f3f62d046e0f6
Sparrow <149317656+Sparrow3699@users.noreply.github.com>
2024-02-22 20:09:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -81,7 +81,7 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait. 5 min Your Files On The Way")
+        temp_msg = await message.reply("Please wait. 5 min Bitlu Videos On The Way")
         try:
             messages = await get_messages(client, ids)
         except:
