49fda8732ab80ac4b0a4afce2ea8ab1a2dd39cc9
Sparrow <149317656+Sparrow3699@users.noreply.github.com>
2024-02-24 20:45:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join This 2 Channels to get files Bitlu vasthayi Join Avvu Fast Ga\n\nPlease I Kindly Request Join Both Channels Now</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>You need to join This 3 Channels to get files Bitlu vasthayi Join Avvu Fast Ga\n\nPlease I Kindly Request Join All 3 Channels Now👇👇👇</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
