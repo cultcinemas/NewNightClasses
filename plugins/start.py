555a36a41b4311f6c2a5e02e7756de992a26303b
GHLuckM4n <luckm4n@outlook.com>
2024-06-23 09:25:18 +0700

    Fix

--- plugins/start.py
+++ plugins/start.py
@@ -92,7 +92,7 @@ async def start_command(client: Client, message: Message):
             return
         await temp_msg.delete()
 
-        messages = []
+        msgs = []
         for msg in messages:
 
             if bool(CUSTOM_CAPTION) & bool(msg.document):
@@ -107,17 +107,17 @@ async def start_command(client: Client, message: Message):
 
             try:
                 cmsg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                messages.append(cmsg)
+                msgs.append(cmsg)
                 await asyncio.sleep(0.5)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
                 cmsg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
-                messages.append(cmsg)
+                msgs.append(cmsg)
             except:
                 pass
         del_msg = await message.reply_text(AUTO_DELETE_MESSAGE, quote=True)
-        messages.append(del_msg)
-        loop.create_task(auto_delete_message(messages))
+        msgs.append(del_msg)
+        loop.create_task(auto_delete_message(msgs))
         return
     else:
         reply_markup = InlineKeyboardMarkup(
