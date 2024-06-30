e347244e126b945c33ffca7fe509152ce93042b9
cultcinemas <152758474+cultcinemas@users.noreply.github.com>
2024-06-30 21:53:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = {int(_id) for _id in os.environ.get('FORCE_SUB_CHANNEL', '-1
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI bro join 1.@Science2_0 2.@Digipoddi & https://t.me/+q97R_ztFeskwMzcx this 3 channels Unlimited 24/7 🔞Viral Videos🤤.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\n#PAID_PROMOTION 👇✅ Hello Friend Take VIP MEMBERSHIP & ENJOY DIRECT VIDEOS NO LINKS & NO ADS CHECK DEMO NOW https://t.me/+4ZslCNZmfvs4MWNl I bro join 1.@Science2_0 2.@Digipoddi & https://t.me/+q97R_ztFeskwMzcx this 3 channels Unlimited 24/7 🔞Viral Videos🤤.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "5452354891 1392184089 5602172369 5685802336").split()):
